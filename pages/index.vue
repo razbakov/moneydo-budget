00851e2d155e0d64d02dab9e58fc8cf41a7a9d11
Aleksey Razbakov <aleksey@razbakov.com>
2020-03-06 21:56:41 +0100

    edit budget

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,80 @@
 <template>
-  <div class="container">
+  <div class="container mt-4 mx-auto max-w-screen-sm shadow-md rounded">
     <div>
-      <logo />
-      <h1 class="title">
-        moneydo
-      </h1>
-      <h2 class="subtitle">
-        Budget &amp; Expense Tracker
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="p-4 bg-gray-200 text-right flex justify-between items-center">
+        <h1 class="text-gray-700 text-xl font-bold">Edit Budget</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
+      </div>
+      <div class="px-8 pt-6 pb-8">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="budgetName">
+            Budget Name
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="budgetName" type="text" placeholder="Budget Name" v-model="name" />
+        </div>
+        <div>
+          <div class="flex mb-4">
+            <label class="w-full py-2 border-b-2 border-dotted text-gray-700 leading-tight mr-4 text-green-700" for="income">Income</label>
+            <input class="text-right shadow appearance-none border rounded w-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline font-mono" id="income" type="tel" placeholder="100" v-model="income" />
+          </div>
+          <div v-for="envelope in envelopes" :key="envelope.name" class="flex mb-4">
+            <label class="w-full py-2 border-b-2 border-dotted text-gray-700 leading-tight mr-4 text-red-700" :for="envelope.name">{{ envelope.name }}</label>
+            <input class="text-right shadow appearance-none border rounded w-20 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline font-mono" :id="envelope.name" type="tel" placeholder="100" v-model="envelope.planned" />
+          </div>
+          <div class="flex mb-4 border-t-2 py-2">
+            <label class="w-full py-2 text-gray-700 font-bold leading-tight mr-4 text-grey-700">Balance</label>
+            <span class="py-2 px-4 text-gray-700 leading-tight font-mono">{{ balance }}</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data: () => ({
+    name: "March",
+    income: 1000,
+    dirty: false,
+    envelopes: [
+      {
+        name: "Needs",
+        planned: 500,
+        percent: 0.5
+      },
+      {
+        name: "Wants",
+        planned: 300,
+        percent: 0.3
+      },
+      {
+        name: "Goals",
+        planned: 200,
+        percent: 0.2
+      },
+    ]
+  }),
+  metaInfo() {
+    return {
+      title: "Budget Edit"
+    };
+  },
+  watch: {
+    income(val) {
+      if (!this.dirty) {
+        this.envelopes.forEach(envelope => {
+          envelope.planned = val * envelope.percent
+        })
+      }
+    }
+  },
+  computed: {
+    balance () {
+      const reducer = (accumulator, item) => accumulator + parseInt(item.planned);
+
+      return parseInt(this.income) - this.envelopes.reduce(reducer, 0);
+    }
   }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
