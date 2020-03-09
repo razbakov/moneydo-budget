ed868f3eb0326b2b5aec10f2f7b43a03cdbb06c6
Aleksey Razbakov <aleksey@razbakov.com>
2020-03-09 10:20:12 +0100

    fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Interactive guide for Kakeibo and 50/30/20' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
